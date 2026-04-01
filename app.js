e0b94704074a505edc060e3ab392e830fc99f942
suliko87sg-cmd <suliko87.sg@gmail.com>
2026-04-01 20:47:40 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -237,7 +237,6 @@ function initFloors() {
 // НАЗАД
 // =====================
 backBtn.onclick = function () {
-  currentBlock = null;
   plan.data = "blocks.svg";
 
   floorPanel.style.display = "none";
