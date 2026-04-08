db94a01b162dbeb8373133979f96d3a0ae74f240
suliko87sg-cmd <suliko87.sg@gmail.com>
2026-04-08 17:27:23 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
+const CACHE_NAME = "app-cache-v1";
+
+const urlsToCache = [
+  "/building-plan/",
+  "/building-plan/index.html",
+  "/building-plan/app.js",
+  "/building-plan/manifest.json",
+  "/building-plan/logo.png"
+];
+
 self.addEventListener("install", e => {
-  console.log("SW installed");
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
