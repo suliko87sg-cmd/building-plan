25031178eef07fb2fbe0bab43c6abef5e1524061
suliko87sg-cmd <suliko87.sg@gmail.com>
2026-04-08 16:21:50 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -300,3 +300,17 @@ function hideFlatCard() {
 }
 
 window.hideFlatCard = hideFlatCard;
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    const splash = document.getElementById("splash");
+
+    splash.style.transition = "opacity 1s";
+    splash.style.opacity = "0";
+
+    setTimeout(() => {
+      splash.style.display = "none";
+    }, 1000);
+
+  }, 4000);
+});
