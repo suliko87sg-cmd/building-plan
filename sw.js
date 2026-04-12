4bbaac5a82381cdccb0331ac6268a9244e835099
suliko87sg-cmd <suliko87.sg@gmail.com>
2026-04-12 11:31:41 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,39 @@
-const CACHE_NAME = "app-cache-v1";
+const CACHE_NAME = "app-v2";
 
 const urlsToCache = [
-  "/building-plan/",
-  "/building-plan/index.html",
-  "/building-plan/app.js",
-  "/building-plan/manifest.json",
-  "/building-plan/logo.png"
+  "./",
+  "./index.html",
+  "./app.js",
+  "./style.css",
+  "./logo.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
   );
 });
