ba18a3f602d0e5c369f84ab5d998f683fdde2b85
suliko87sg-cmd <suliko87.sg@gmail.com>
2026-05-04 02:54:28 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "app-cache-v1";
+const CACHE_NAME = "app-cache-v2";
 
 const urlsToCache = [
   "/building-plan/",
@@ -8,15 +8,21 @@ const urlsToCache = [
   "/building-plan/logo.png"
 ];
 
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      return Promise.all(
+        urlsToCache.map(url => {
+          return cache.add(url).catch(() => {
+            console.log("❌ не закэшировался:", url);
+          });
+        })
+      );
     })
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then(response => {
       return response || fetch(e.request);
